1b2f6f9476e4885485dc96e8b7f69bb6501c0f16
Huy <laiquanghuy24122001@gmail.com>
2023-10-11 18:21:35 +0700

    .

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -11,12 +11,12 @@
         class="bg-cover bg-top bg-no-repeat lg:bg-[image:url('/src/assets/bg_desktop.svg')] bg-[image:url('/src/assets/bg_mobile.svg')] h-full">
         <LightSwitch class="opacity-5" />
         <div class="flex items-center justify-center">
-            <img src="/src/assets/logo1.svg" alt="" width="110" class="mx-2" />
-            <img src="/src/assets/logo2.svg" alt="" width="60" class="mx-2" />
-            <img src="/src/assets/logo3.svg" alt="" width="80" class="mx-2" />
+            <img src={'/src/assets/logo1.svg'} alt="" width="110" class="mx-2" />
+            <img src={'/src/assets/logo2.svg'} alt="" width="60" class="mx-2" />
+            <img src={'/src/assets/logo3.svg'} alt="" width="80" class="mx-2" />
         </div>
         <div class="container flex items-center justify-center h-full mx-auto">
-            <div class="flex flex-col items-center -mt-64 space-y-10 text-center">
+            <div class="flex flex-col items-center space-y-10 text-center">
                 <slot />
             </div>
         </div>
